7a123275d869c5d7c0225c5a60f51f74708d3915
longen0707 <gdszzyjsxy@hotmail.com>
2018-02-05 16:22:00 +0800

    修改的内容

--- src/com/youmeek/demo/HelloWorld.java
+++ src/com/youmeek/demo/HelloWorld.java
@@ -3,6 +3,6 @@ package com.youmeek.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("hello hello");
+        System.out.println("这个是什么啊");
     }
 }

45217940a76d2cc751e38a9a1b89f59f565d3e62
luosidin <gdszzyjsxy@hotmail.com>
2018-02-05 16:18:46 +0800

    Update HelloWorld.java

--- src/com/youmeek/demo/HelloWorld.java
+++ src/com/youmeek/demo/HelloWorld.java
@@ -3,5 +3,6 @@ package com.youmeek.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("hello hello");
     }
 }
